948b1d79cbdf93ba89dcfe60556148d977f621b0
Dario <dariokleinalves@gmail.com>
2024-06-18 16:23:04 -0300

    fix user-slice error

--- src/redux/user/user-slice.ts
+++ src/redux/user/user-slice.ts
@@ -20,7 +20,7 @@ export const userSlice = createSlice({
     login: (state, action) => {
       state.user = action.payload
     },
-    logout: (state, action) => {
+    logout: (state) => {
       state.user = null
     }
   }
